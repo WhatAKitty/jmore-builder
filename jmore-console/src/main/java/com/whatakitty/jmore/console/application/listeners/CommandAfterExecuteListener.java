37d5a6bcedfd525483a2c487f6beb1c7a3865c4c
WhatAKitty <104xuqiang@163.com>
2019-05-02 23:55:37 +0800

    Add into history while command execute

---
+++ jmore-console/src/main/java/com/whatakitty/jmore/console/application/listeners/CommandAfterExecuteListener.java
@@ -0,0 +1,38 @@
+package com.whatakitty.jmore.console.application.listeners;
+
+import com.whatakitty.jmore.console.ConsoleContext;
+import com.whatakitty.jmore.console.domain.command.ICommand;
+import com.whatakitty.jmore.console.domain.command.event.CommandAfterExecuteEvent;
+import com.whatakitty.jmore.console.domain.history.History;
+import com.whatakitty.jmore.console.domain.history.HistoryFactory;
+import com.whatakitty.jmore.framework.bootstrap.listener.JMoreApplicationListener;
+import com.whatakitty.jmore.framework.compilerule.annotations.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * add history after command execute successfully
+ *
+ * @author WhatAKitty
+ * @date 2019/05/02
+ * @description
+ **/
+@RequiredArgsConstructor
+@Component
+public class CommandAfterExecuteListener implements JMoreApplicationListener<CommandAfterExecuteEvent> {
+
+    private final HistoryFactory historyFactory;
+
+    @Override
+    public void onApplicationEvent(@NotNull CommandAfterExecuteEvent event) {
+        ConsoleContext context = event.getContext();
+        ICommand command = event.getCommand();
+
+        // get or create history
+        History history = historyFactory.getOrCreateHistory(context);
+
+        // add command into history
+        history.addHistory(command);
+    }
+
+}
---
+++ jmore-console/src/main/java/com/whatakitty/jmore/console/application/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author WhatAKitty
+ * @date 2019/05/02
+ * @description
+ **/
+
+package com.whatakitty.jmore.console.application;
