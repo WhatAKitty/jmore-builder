de406e530938936b072df583df4160c5a31dcd63
WhatAKitty <104xuqiang@163.com>
2020-06-19 08:52:43 +0800

    Add response status code

--- jmore-web/src/main/java/com/whatakitty/jmore/web/api/ApiAdvice.java
+++ jmore-web/src/main/java/com/whatakitty/jmore/web/api/ApiAdvice.java
@@ -4,12 +4,14 @@ import com.whatakitty.jmore.framework.validation.ValidationMsg;
 import com.whatakitty.jmore.web.resultcode.system.SystemExecutionResultCode;
 import com.whatakitty.jmore.web.resultcode.user.UserRequestParamsResultCode;
 import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,33 +26,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(annotations = RestController.class)
 public final class ApiAdvice {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentNotValidException.class})
     private Object handlemethodArgumentNotValidException(MethodArgumentNotValidException e) {
         return handleBindingResultException(e.getBindingResult());
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({BindException.class})
     private Object handleBadRequestException(BindException e) {
         return handleBindingResultException(e.getBindingResult());
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({HttpMessageNotReadableException.class})
     private Object handleHttpMessageNotReadableException() {
         return Result.getResult(UserRequestParamsResultCode.USER_REQUEST_PARAMS_ERROR, "missing body");
     }
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Throwable.class})
     private Object handleUncaughtException(Throwable t) {
         return Result.getResult(SystemExecutionResultCode.SYSTEM_EXECUTION_EXCEPTION, t.getMessage());
     }
 
     private Object handleBindingResultException(BindingResult bindingResult) {
-        return Result.getResult(
-            UserRequestParamsResultCode.USER_REQUEST_PARAMS_ERROR,
-            bindingResult.getAllErrors().stream()
-                .map(ValidationMsg::new)
-                .collect(Collectors.toSet())
-        );
+        Result<Object> result = Result.getResult(UserRequestParamsResultCode.USER_REQUEST_PARAMS_ERROR, null);
+        result.addDetailedMsgs(bindingResult.getAllErrors().stream()
+            .map(ValidationMsg::new)
+            .collect(Collectors.toSet()));
+        return result;
     }
 
 }
