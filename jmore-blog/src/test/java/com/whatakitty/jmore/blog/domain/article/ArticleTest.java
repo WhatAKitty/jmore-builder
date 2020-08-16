05864d6769bad724ba1e96db3f407d6de950f710
WhatAKitty <104xuqiang@163.com>
2020-08-16 23:33:49 +0800

    Fix test unit of ArticleTest

--- jmore-blog/src/test/java/com/whatakitty/jmore/blog/domain/article/ArticleTest.java
+++ jmore-blog/src/test/java/com/whatakitty/jmore/blog/domain/article/ArticleTest.java
@@ -1,9 +1,15 @@
 package com.whatakitty.jmore.blog.domain.article;
 
 import com.whatakitty.jmore.framework.ddd.publishedlanguage.AggregateId;
+import com.whatakitty.jmore.framework.utils.SpringUtils;
+import java.lang.reflect.Field;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -15,6 +21,13 @@ import org.springframework.test.context.junit4.SpringRunner;
  **/
 @RunWith(SpringRunner.class)
 public class ArticleTest {
+
+    @Before
+    public void init() throws IllegalAccessException {
+        final ApplicationContext applicationContext = Mockito.mock(ApplicationContext.class);
+        final Field setApplicationContext = PowerMockito.field(SpringUtils.class, "applicationContext");
+        setApplicationContext.set(SpringUtils.class, applicationContext);
+    }
 
     @Test
     public void test_publish_normal() {
