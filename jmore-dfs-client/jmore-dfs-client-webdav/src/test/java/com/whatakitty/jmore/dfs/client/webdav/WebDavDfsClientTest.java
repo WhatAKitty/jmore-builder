4e8bd09f79227b9afe4759baac2c10f1d0b10cb7
WhatAKitty <104xuqiang@163.com>
2020-08-17 00:09:50 +0800

    Fix test unit for WebDavDfsClientTest

--- jmore-dfs-client/jmore-dfs-client-webdav/src/test/java/com/whatakitty/jmore/dfs/client/webdav/WebDavDfsClientTest.java
+++ jmore-dfs-client/jmore-dfs-client-webdav/src/test/java/com/whatakitty/jmore/dfs/client/webdav/WebDavDfsClientTest.java
@@ -1,11 +1,20 @@
 package com.whatakitty.jmore.dfs.client.webdav;
 
-import java.io.*;
+import com.github.sardine.Sardine;
+import com.github.sardine.SardineFactory;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,6 +23,8 @@ import org.junit.Test;
  * @date 2020/07/09
  * @since 1.0.0
  **/
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({SardineFactory.class, FileUtils.class})
 public class WebDavDfsClientTest {
 
     private String endpoint;
@@ -22,16 +33,30 @@ public class WebDavDfsClientTest {
     private String testFilePath;
 
     @Before
-    public void init() {
-        this.endpoint = System.getenv("DFS_ENDPOINT");
-        this.accessKey = System.getenv("DFS_ACCESS_KEY");
-        this.accessSecret = System.getenv("DFS_ACCESS_SECRET");
-        this.testFilePath = System.getenv("DFS_TEST_FILE_PATH");
+    public void init() throws IOException {
+//        this.endpoint = System.getenv("DFS_ENDPOINT");
+//        this.accessKey = System.getenv("DFS_ACCESS_KEY");
+//        this.accessSecret = System.getenv("DFS_ACCESS_SECRET");
+//        this.testFilePath = System.getenv("DFS_TEST_FILE_PATH");
+        this.endpoint = "http://xx.com";
+        this.accessKey = "";
+        this.accessSecret = "";
+        this.testFilePath = "";
+
+        // init web dfs client
+        final Sardine sardine = PowerMockito.mock(Sardine.class);
+        PowerMockito.when(sardine.exists(Mockito.anyString())).thenReturn(true);
+
+        PowerMockito.mockStatic(SardineFactory.class);
+        PowerMockito.when(SardineFactory.begin(this.accessKey, this.accessSecret)).thenReturn(sardine);
+
+        PowerMockito.mockStatic(FileUtils.class);
+        PowerMockito.when(FileUtils.readFileToByteArray(Mockito.any())).thenReturn(new byte[0]);
     }
 
 
     @Test
-    public void test_put() throws FileNotFoundException, ExecutionException, InterruptedException {
+    public void test_put() throws IOException, ExecutionException, InterruptedException {
         WebDavProperties webDavProperties = new WebDavProperties();
         webDavProperties.setType("webdav");
         webDavProperties.setEndpoint(this.endpoint);
@@ -40,9 +65,11 @@ public class WebDavDfsClientTest {
         WebDavDfsClient webDavDfsClient = new WebDavDfsClient(webDavProperties);
         webDavDfsClient.init();
 
+        final byte[] bytes = FileUtils.readFileToByteArray(new File(this.testFilePath));
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
         WebDavObjectKey objectKey = new WebDavObjectKey("aa", new String[] {"test"});
         WebDavObject webDavObject = new WebDavObject(objectKey);
-        webDavObject.setInputStream(new FileInputStream(new File(this.testFilePath)));
+        webDavObject.setInputStream(byteArrayInputStream);
 
         final Boolean result = webDavDfsClient.putObject(webDavObject, false).get();
         Assert.assertTrue(result);
